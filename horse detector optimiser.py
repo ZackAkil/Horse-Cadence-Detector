da1c415a51a16ee2076fad15d596c0f9a1f5d4bd
ZackAkil <zackakil94@gmail.com>
2016-07-16 14:48:17 +0100

    embedded multi label output from model

--- horse detector optimiser.py
+++ horse detector optimiser.py
@@ -1,31 +1,45 @@
+from __future__ import division
 from scipy import optimize
 import matplotlib.pyplot as plt
 import numpy as np
 
-def model(peakThresholdMin, peakFrequencyThresholdMin,sampleWindow, data):
+def model(peakCantThreshold,peakTrotThreshold, peakCantFrequencyThreshold, peakTrotFrequencyThreshold,sampleWindow, data):
     
-    inPeakFlag = False
-    validPeaks = 0
+    inTrotPeakFlag = False
+    inCantPeakFlag = False
+    validTrotPeaks = 0
+    validCantPeaks = 0
     time = 0;
     output = []
     
     for row in data:
         time += 1
         #enter peak
-        if(row > peakThresholdMin) and (not inPeakFlag):
-            inPeakFlag = True
-            #print("peaked")
-        #exit peak
-        elif(row < peakThresholdMin) and inPeakFlag:
-            inPeakFlag = False
-            #count if peak width is valid
-            validPeaks += 1
+        if(row > peakTrotThreshold) and (not inTrotPeakFlag):
+            inTrotPeakFlag = True
+        elif(row < peakTrotThreshold) and inTrotPeakFlag:
+            inTrotPeakFlag = False
+            validTrotPeaks += 1
+            
+        if(row > peakCantThreshold) and (not inCantPeakFlag):
+            inCantPeakFlag = True
+        elif(row < peakCantThreshold) and inCantPeakFlag:
+            inCantPeakFlag = False
+            validCantPeaks += 1
+        
         #after sample wind passes log prediction to array
         if(time > sampleWindow):
-            label = 1 if(peakFrequencyThresholdMin < validPeaks) else 0
+            
+            label = 0
+            if(validCantPeaks > peakCantFrequencyThreshold):
+                label = 2
+            elif(validTrotPeaks > peakTrotFrequencyThreshold):
+                label = 1
+            
             for i in range(int(sampleWindow)):
                 output.append(label)
-            validPeaks = 0
+            validTrotPeaks = 0
+            validCantPeaks = 0
             time = 0
     while(len(output) < len(data)):
         output.append(0)
@@ -41,24 +55,27 @@ def fetchTrainingData():
 def costFunction(theta):
     prediction = predict(theta)
     #actual = np.array([0,1,1,1,0,1,1])
-    actual =  trainingData[:,2] + trainingData[:,1] 
+    actual =  (trainingData[:,2] *2) + trainingData[:,1] 
     #print("pred",sum(prediction), sum(actual))
-    absCost = np.absolute(prediction - actual)
+    absCost = ((np.absolute(prediction - actual)) >= 1).astype(int)
+
+        
+    
     finalCost = sum(absCost)/len(absCost)
     #print('ran',finalCost,theta)
     progress.append(finalCost)
     return finalCost
 
 def optimizeTheta():
-    sol = optimize.minimize(costFunction,x0=[0.0001,0.1],method='Powell')#Nelder-Mead
+    sol = optimize.minimize(costFunction,x0=[0.00001,0.00001,0.01,0.01],method='Powell',options={'maxiter' : 100000})#Nelder-Mead
     return sol
 
 def predict(theta):
     scaledTheta = scaleFeatures(theta)
-    return model(scaledTheta[0],scaledTheta[1],100,trainingData[:,0])
+    return model(scaledTheta[0],scaledTheta[1],scaledTheta[2],scaledTheta[3],100,trainingData[:,0])
     
 def scaleFeatures(theta):
-    return [theta[0]*70000000,theta[1]*30]
+    return [theta[0]*700000000,theta[1]*700000000,theta[2]*300,theta[3]*300]
     
 
 trainingData = fetchTrainingData()
@@ -85,7 +102,7 @@ for i in range(num):
 #plt.axhline(y=finalTop)
 #plt.axhline(y=finalBottom)
 output = scaleFeatures(sol.x)
-#plt.axhline(y=finalTop)
+plt.axhline(y=output[0])
 #plt.axhline(y=finalBottom)
 print("bttom line",finalBottom)
 
